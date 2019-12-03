612cfccf93da543bed65657d04fb514435d57622
HukuToc46 <hukutoc46@gmail.com>
2019-12-03 21:59:14 +0300

    base objects

--- backend/objects.py
+++ backend/objects.py
@@ -1,22 +1,31 @@
 import numpy as np
 
 MAX_COORDINATE = 16
+RADIUS_NOISE = 0.1
+COORDINATE_NOISE = 0.2
+EPS = 1e-10
+
+#######################################################################################################################
+
+def normal(lower=None, upper=None):
+    if lower is None and upper is None:
+        return np.random.normal()
+    elif lower is None:
+        return min(np.random.normal(), upper)
+    elif upper is None:
+        return max(np.random.normal(), lower)
+    else:
+        return max(min(np.random.normal(), upper), lower)
 
 class BaseObject:
     """
     Базовый объект, который представляет из себя программу
     """
-    def TikZ(self):
+    def TikZ(self, noisy=False):
         """
         :return: строку-программу в TikZ формате
         """
-        return "\n".join(self.evaluate())
-
-    def noisyTikZ(self):
-        """
-        :return: строку-программу в TikZ формате
-        """
-        return "\n".join(self.noisyEvaluate())
+        return "\n".join(self.to_command(noisy))
 
     def __eq__(self, other):
         return str(self) == str(other)
@@ -29,3 +38,82 @@ class BaseObject:
 
     def __ne__(self, other):
         return str(self) != str(other)
+
+    def to_command(self, noisy=False):
+        assert False
+        return ''
+
+
+#######################################################################################################################
+
+
+class Point(BaseObject):
+    @staticmethod
+    def sample():
+        return np.random.randint(1, MAX_COORDINATE - 1)
+
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def make_divisible_by(self, denominator):
+        return Point(round(self.x / denominator) * denominator,
+                     round(self.y / denominator) * denominator)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, coeff):
+        return Point(self.x * coeff, self.y * coeff)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __str__(self):
+        return "(%s, %s)" % (str(self.x), str(self.y))
+
+    def to_command(self, noisy=False):
+        if noisy:
+            return str(Point(self.x + normal() * COORDINATE_NOISE, self.y + normal() * COORDINATE_NOISE))
+        else:
+            return str(self)
+
+
+class Line(BaseObject):
+    def __init__(self, p1, p2, is_arrow=False, is_solid=True):
+        self.point_from = p1
+        self.point_to = p2
+        self.is_arrow = is_arrow
+        self.is_solid = is_solid
+
+    def
+
+
+#######################################################################################################################
+
+
+def main():
+    for _ in range(100):
+        lower = -0.1
+        upper = 0.1
+        assert normal(lower=lower) > lower - EPS
+        assert normal(upper=upper) < upper + EPS
+        assert lower - EPS < normal(lower=lower, upper=upper) < upper + EPS
+
+    p1 = Point(5, 6)
+    p2 = Point(3, 4)
+    mul = 2
+    assert str(p1) == p1.to_command() == '(5, 6)'
+    assert p1 == p1
+    assert p1 + p2 == Point(8, 10)
+    assert p1 - p2 == Point(2, 2)
+    assert p1 * mul == Point(10, 12)
+    assert p1.make_divisible_by(mul) == Point(4, 6)
+
+    print('All passed')
+
+
+if __name__ == '__main__':
+    main()
